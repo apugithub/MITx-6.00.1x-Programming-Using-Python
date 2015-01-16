05eef8d14f9483c1518b1042eef5b08c862a9ca4
apugithub <apugithub@users.noreply.github.com>
2015-01-16 10:00:51 +0530

    Update L3Problem9.py
    Program that guesses a secret number using Bisection Method

--- Week2/L3Problem9.py
+++ Week2/L3Problem9.py
@@ -10,7 +10,7 @@ while str(x) != 'c':
     elif str(x) == 'l':
         low = ans
     else:
-        print 'Sorry, I did not understand your input.'
+        print 'Sorry, I did not understand your input..'
     ans = abs((low + high) / 2)
     print 'Is your secret number ' + str(ans) + ' ?'
     x = raw_input("Enter 'h' to indicate the guess is too high.       Enter 'l' to indicate the guess is too low. Enter 'c' to      indicate I guessed correctly.")
