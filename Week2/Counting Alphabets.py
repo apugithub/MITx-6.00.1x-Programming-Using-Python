2801c213f2d0d91bef328ea0d7dd8ddb3a5aca0c
apugithub <apugithub@users.noreply.github.com>
2015-01-18 14:33:37 +0530

    Create Counting Alphabets.py
    Counting alphabets in a string...two programes

---
+++ Week2/Counting Alphabets.py
@@ -0,0 +1,21 @@
+# First Programe:
+
+s= 'abnghdfteryt'
+def count_vowels(s):
+    vowels = 'aeiou'
+    counter = 0
+    for vowel in s:
+        if vowel in vowels:
+            counter += 1
+    return counter
+print count_vowels(s)
+
+
+# Second Program:
+
+word = str(raw_input("Enter a word: "));
+numVowel = 0
+for vowel in word:
+    if vowel == "a" or vowel == "e" or vowel == "i" or vowel == "o" or vowel == "u":
+        numVowel += 1
+print("Number of vowels: " + str(numVowel))
