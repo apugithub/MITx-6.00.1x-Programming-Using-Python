659cdd705c78b27eee6da6ad403b14f0f9468990
apugithub <apugithub@users.noreply.github.com>
2015-01-25 00:20:04 +0530

    Create L4 Problem 11.py
    Define a function isVowel2(char) that returns True if char is a vowel ('a', 'e', 'i', 'o', or 'u'), and False otherwise. You can assume that char is a single letter of any case (ie, 'A' and 'a' are both valid).
    
    This function is similar to the previous problem - but this time, do use the keyword in. Your function should take in a single string and return a boolean.

---
+++ Week2/L4 Problem 11.py
@@ -0,0 +1,9 @@
+vowel= {'a','e','i','o','u','A','E','I','O','U'}
+def isVowel2(char):
+    
+    '''
+    char: a single letter of any case
+
+    returns: True if char is a vowel and False otherwise.
+    '''
+    return ( char in vowel)
