93a6d6f0fabae683f0b6a788077314cabdec9531
apugithub <apugithub@users.noreply.github.com>
2015-01-25 21:24:44 +0530

    Update Hangman_Is The Word Guessed.py

--- Week-3/Hangman_Is The Word Guessed.py
+++ Week-3/Hangman_Is The Word Guessed.py
@@ -25,27 +25,3 @@ def isWordGuessed(secretWord, lettersGuessed):
             return False
             
             
-
-
-###### 3rd Approach:
-
-def getGuessedWord(secretWord, lettersGuessed):
-    '''
-    secretWord: string, the word the user is guessing
-    lettersGuessed: list, what letters have been guessed so far
-    returns: string, comprised of letters and underscores that represents
-      what letters in secretWord have been guessed so far.
-    '''
-    # FILL IN YOUR CODE HERE...
-    res = ''
-    for i in secretWord:
-        if i in lettersGuessed:
-            res += i
-        else:
-            res += "_ "
-    return res
-
-
-
-
-            
