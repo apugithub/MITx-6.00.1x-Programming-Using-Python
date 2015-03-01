b2dcc2d3d1139c53ed0b65f68070dc2b2407b793
apugithub <apugithub@users.noreply.github.com>
2015-03-01 21:00:43 +0530

    PROBLEM 2: DECRYPTION (DECRYPTSTORY)

---
+++ Week-6/PROBLEM 2: DECRYPTION (DECRYPTSTORY).py
@@ -0,0 +1,28 @@
+def decryptStory():
+    """
+    Using the methods you created in this problem set,
+    decrypt the story given by the function getStoryString().
+    Use the functions getStoryString and loadWords to get the
+    raw data you need.
+
+    returns: string - story in plain text
+    """
+    
+    wordList = loadWords()
+    story = getStoryString()
+    bestShift = findBestShift(wordList, story)
+    return applyShift(story, bestShift)
+    
+
+#
+# Build data structures used for entire session and run encryption
+#
+
+if __name__ == '__main__':
+    # To test findBestShift:
+    wordList = loadWords()
+    s = applyShift('Hello, world!', 8)
+    bestShift = findBestShift(wordList, s)
+    assert applyShift(s, bestShift) == 'Hello, world!'
+    # To test decryptStory, comment the above four lines and uncomment this line:
+    #    decryptStory()
